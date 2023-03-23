6a06eafd7a2bf53d10375a9e3644fd344f915a0c
Kirk Hauck <kirkihauck@gmail.com>
2023-03-23 16:05:20 -0600

    feat: Update App state using mock data in componentDidMount

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import movieMockData from '../../movieData/movieData';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      movieData: {},
+      selectedMovie: {}
+    }
   }
   
+  componentDidMount = () => {
+    this.setState({ movieData: movieMockData });
+  }
+
   render() {
     return (
       <h1>Hello world!</h1>
