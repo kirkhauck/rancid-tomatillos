3edc1a198099e0a36208947dbcdf6a4bc28df488
Alexys Jordan <alexyskjordan@gmail.com>
2023-03-23 16:06:14 -0600

    Feature header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css"
+
+const Header = () => {
+  <div className="header-section">
+    <img src="src/Assets/tomatillo.png"></img>
+    <h1>Rancid Tomatillos</h1>
+    </div>
+}
+export default Header;
