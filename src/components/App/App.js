faac60888b13b4cd922d368fd69982d9b4ce8417
Alexys Jordan <alexyskjordan@gmail.com>
2023-03-24 16:46:19 -0600

    Add skeleton to singlemovie components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Header from '../Header/Header';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import movieMockData from '../../movieData/movieData';
+// import singleMovieData from '../singleMovieData1/singleMovieData';
+// import singleMovieData from '../singleMovieData2/singleMovieData';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +11,7 @@ class App extends Component {
     super();
     this.state = {
       movieData: '',
+      // singleMovie: ''
       selectedMovie: ''
     }
   }
