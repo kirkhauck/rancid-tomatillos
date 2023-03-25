4b78dbb28966446102657bd888b299c79c500552
Kirk Hauck <kirkihauck@gmail.com>
2023-03-25 14:04:53 -0600

    feat: Add PropTypes to SingleMovieBanner

--- src/components/SingleMovieBanner/SingleMovieBanner.js
+++ src/components/SingleMovieBanner/SingleMovieBanner.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './SingleMovieBanner.css';
 
 
 const SingleMovieBanner = ({image}) => {
- 
-    return (
-      <div>
-      <img
-        className="banner"
-        src={image}
-      />
-      </div>
-    );
-  
+  return (
+    <div>
+      <img className="banner" src={image} />
+    </div>
+  );
+}
+
+SingleMovieBanner.propTypes = {
+  image: PropTypes.string.isRequired
 }
 
 export default SingleMovieBanner;
