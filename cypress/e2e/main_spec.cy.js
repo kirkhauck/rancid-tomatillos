8e36de2c41f6f0c51c509c9dc7f05fcb23dc6ce0
Kirk Hauck <kirkihauck@gmail.com>
2023-03-27 17:25:18 -0600

    feat: Add initial main page tests

--- cypress/e2e/main_spec.cy.js
+++ cypress/e2e/main_spec.cy.js
@@ -7,4 +7,11 @@ describe('Rancid Tomatillos', () => {
     cy.get('.header').find('img');
     cy.get('.header').contains('h1', 'Rancid Tomatillos');
   });
+
+  it('Should show thumbnails of all the movies', () => {
+    cy.get('.movie-card').should('have.length', 40)
+    cy.get('.movie-card').find('img');
+    cy.get('.movie-card').find('h3');
+    cy.get('.movie-card').find('p');
+  });
 });
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -7,11 +7,11 @@ const MovieCard = ({id, title, average_rating, release_date, poster_path, select
 
   console.log(selectMovie, "hi")
   return (
-    <div className='movieCard' onClick={() => selectMovie(id)}>
-      <img className='movieCard' src={poster_path}/>
-     <h3>{title}</h3>
+    <div className='movie-card' onClick={() => selectMovie(id)}>
+      <img className='movie-img' src={poster_path}/>
+      <h3>{title}</h3>
       <p>{average_rating}</p>
-      <>{release_date}</>
+      <p>{release_date}</p>
     </div>
   )
 }
