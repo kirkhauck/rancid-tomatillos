e1b2f6bad2646fb069f7c1e695766edcce291e72
Alexys Jordan <alexyskjordan@gmail.com>
2023-03-29 18:09:30 -0600

    Add stubs to main and details testing spec

--- cypress/e2e/details_spec.cy.js
+++ cypress/e2e/details_spec.cy.js
@@ -1,10 +1,16 @@
-// describe('Rancid Tomatillos', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/436270');
-//   });
-//   it('Should show the details page with the header', () => {
-//     cy.get('.header').find('img');
-//     cy.get('.header').contains('h1', 'Rancid Tomatillos');
-//   });
+describe('Rancid Tomatillos Details Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.loadPage()
+    cy.get('.movie-card').contains('h3', 'Black Adam').click()
+    cy.loadSingleMovie()
+  });
 
-// })
+     it('Should show the details page with the header', () => {
+     cy.get('.header').find('img');
+     cy.get('.header').contains('h1', 'Rancid Tomatillos');
+   });
+   
+   it('Should have details')
+
+  })
