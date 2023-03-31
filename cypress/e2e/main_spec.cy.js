f3c9dd754c362e6a1c9439bdc2b9cb546d15984e
Kirk Hauck <kirkihauck@gmail.com>
2023-03-31 13:59:17 -0600

    feat: Update main_spec to account for date change

--- cypress/e2e/main_spec.cy.js
+++ cypress/e2e/main_spec.cy.js
@@ -21,25 +21,25 @@ describe('Main Page', () => {
     cy.get('.movie-card:first()').find('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg')
       .get('.movie-card:first()').contains('h3', 'Black Adam')
       .get('.movie-card:first()').find('p:first()').contains('4')
-      .get('.movie-card:first()').find('p:last()').contains('2022-10-19');
+      .get('.movie-card:first()').find('p:last()').contains('Released: 10/19/2022');
 
     cy.get('.movie-card:last()').find('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pUPwTbnAqfm95BZjNBnMMf39ChT.jpg')
       .get('.movie-card:last()').contains('h3', 'The Minute You Wake Up Dead')
       .get('.movie-card:last()').find('p:first()').contains('5')
-      .get('.movie-card:last()').find('p:last()').contains('2022-11-04');
+      .get('.movie-card:last()').find('p:last()').contains('Released: 11/04/2022');
   });
 
+describe('Failed Main Page Fetch', () => {
+
   it('Should handle an error when failing to fetch.', () => {
-  cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-    statusCode: 404
-  })
-  })
-
-  it('Should send the user a message when the fetch failed', () => {
-    cy.contains('Not Found!')
-  })
-
-});
+    cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 404
+    });
+      cy.visit('http://localhost:3000');
+      cy.contains('Not Found!');
+    });
+  });
+})
 
 
 
