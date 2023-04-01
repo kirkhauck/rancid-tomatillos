c7e146ae6578e09a370a794713368a57d66dab9a
Kirk Hauck <kirkihauck@gmail.com>
2023-04-01 10:50:58 -0600

    fix: Delete sad tomatillo.png

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,9 @@
 import React from "react";
-import tomatilloImg from "../../assets/tomatillo.png";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className='header'>
-      <img className="logo" src={tomatilloImg} />
       <h1>Rancid Tomatillos</h1>
     </div>
   );
